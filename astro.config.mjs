2d6158d0bfdcd039ea1f303610b9d2812858dcc2
higletgit <higutetsu31415@gmail.com>
2026-07-07 01:58:12 +0900

    Update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://seichi-atlas.vercel.app',
+  site: 'https://seichi-atlas-site.vercel.app',
   integrations: [sitemap()],
 });
